d60114f9f6d05ea52d71027fcf1801a2ee472ece
Zachary Kuchar <zacharyk323@live.com>
2023-08-23 14:49:50 -0400

    Update server to render page

--- server.js
+++ server.js
@@ -1,7 +1,33 @@
 const express = require("express");
 
+const path = require("path");
+const favicon = require("serve-favicon");
+const logger = require("morgan");
+
 const app = express();
 
-app.listen("3001", () => {
-  console.log("Listening on Port 3001");
+app.use(logger("dev"));
+app.use(express.json());
+
+// Configure both serve-favicon & static middleware
+// to serve from the production 'build' folder
+//Set up to access faviocon name icon
+app.use(favicon(path.join(__dirname, "build", "favicon.ico")));
+app.use(express.static(path.join(__dirname, "build")));
+
+// Put API routes here, before the "catch all" route
+
+// The following "catch all" route (note the *) is necessary
+// to return the index.html on all non-AJAX requests
+app.get("/*", function (req, res) {
+  // renders the html file
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
+
+// Configure to use port 3001 instead of 3000 during
+// development to avoid collision with React's dev server
+const port = process.env.PORT || 3001;
+
+app.listen(port, () => {
+  console.log(`Listening on port ${port}`);
 });
