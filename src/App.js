b0a8b02edbb1375f822a16463739f17522288a15
Zachary Kuchar <zacharyk323@live.com>
2023-08-23 16:47:47 -0400

    Changes to render components with Routes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,19 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import { Routes, Route } from "react-router-dom";
+import Auth from "./pages/Auth/Auth";
+import { useState } from "react";
+import Posts from "./pages/Posts/Posts";
 
 function App() {
+  const [user, setUser] = useState(null);
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      This is our app
+      <Routes>
+        <Route path="/" element={<Auth />} />
+        <Route path="/posts" element={<Posts />} />
+      </Routes>
     </div>
   );
 }
