989bb19abb98f2070114132e6cd64b58da8d0e17
Zachary Kuchar <zacharyk323@live.com>
2023-08-23 16:45:47 -0400

    Authentication and posts pages

---
+++ src/pages/Auth/Auth.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Auth() {
+  return (
+    <div>
+      <h1>Authentication page</h1>
+    </div>
+  )
+}
+
+export default Auth
---
+++ src/pages/Posts/Posts.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function Posts() {
+  return (
+    <div>
+      <h1>The overall posts page</h1>
+    </div>
+  )
+}
+
+export default Posts
