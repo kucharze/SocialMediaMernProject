aa1e0521b7b5884e62afb7ebb12ae2b079adf0dd
Zachary Kuchar <zacharyk323@live.com>
2023-08-24 09:38:52 -0400

    Added inputs for email and password

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -4,6 +4,11 @@ function Auth() {
   return (
     <div>
       <h1>Authentication page</h1>
+      <form>
+        <input name='email' type='text' placeholder='Email'/>
+        <input name='password' type='email' placeholder='password'/>
+        <input type='submit'/>
+      </form>
     </div>
   )
 }
