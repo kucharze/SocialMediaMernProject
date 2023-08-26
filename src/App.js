bd8f264c4de7b1393115ab4e8b852e928fe23197
Zachary Kuchar <zacharyk323@live.com>
2023-08-26 11:31:06 -0400

    Adjusted main page based on user login

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from "./logo.svg";
 import "./App.css";
-import { Routes, Route } from "react-router-dom";
+import { Routes, Route, useParams } from "react-router-dom";
 import { useState } from "react";
 import Auth from "./pages/Auth/Auth";
 import Posts from "./pages/Posts/Posts";
@@ -11,11 +11,14 @@ function App() {
   return (
     <div className="App">
       This is our app
-      <Routes>
-        <Route path="/" element={<Auth />} />
-        <Route path="/posts" element={<Posts />} />
-        <Route path="/User/:id" element={<UserPage />} />
-      </Routes>
+      {user ? (
+        <Routes>
+          <Route path="/posts" element={<Posts />} />
+          <Route path="/User/:id" element={<UserPage />} />
+        </Routes>
+      ) : (
+        <Auth />
+      )}
     </div>
   );
 }
