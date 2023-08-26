4cc81d7cb63ceb161e9970abbb39aba49869a87a
Zachary Kuchar <zacharyk323@live.com>
2023-08-26 13:57:06 -0400

    Added classname and submit button

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -2,10 +2,11 @@ import React from 'react'
 
 function CreatePost() {
   return (
-    <div>
+    <div className='createPost'>
       <h1>This will be the form to create a new post</h1>
       <form>
         <input name='post' type='text'/>
+        <input type='submit'/>
       </form>
     </div>
   )
