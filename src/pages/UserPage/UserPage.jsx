0dea8a899e5cf63bd4f0d1c1c6b02bf3ea077403
Zachary Kuchar <zacharyk323@live.com>
2023-08-26 14:20:34 -0400

    Button to display create form post

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,15 +1,21 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
+import CreatePost from '../../components/CreatePost'
 
 function UserPage(props) {
     const id = useParams().id
     const [user,setUser] = useState('user')
+    const [create,setCreate] = useState(false)
       const [posts, setPosts] = useState(['the','overall','posts','for','this','user'])
 
   return (
     <div>
       <h1>The user profile for a particular user</h1>
       <h2>The id: {id}</h2>
+      <button onClick={()=>{setCreate(!create)}}>Create a new post</button>
+      {
+        create &&  <CreatePost/>
+      }
       <ul>
         {
           posts.map((item)=>{
