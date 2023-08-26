825fcb8aa2c6909180e51347921f53593b85305b
Zachary Kuchar <zacharyk323@live.com>
2023-08-26 13:06:07 -0400

    Set up posts display for posts and userprofile pages

--- src/pages/Posts/Posts.jsx
+++ src/pages/Posts/Posts.jsx
@@ -3,13 +3,15 @@ import React , {useState} from 'react'
 function Posts() {
   const [posts, setPosts] = useState(['the','overall','posts','page'])
   return (
-    <div className='posts'>
+    <div className='postList'>
       <h1>The overall posts page</h1>
       <h2>We will display the last so many posts here</h2>
       <ul>
         {
           posts.map((item)=>{
-            return <li>{item}</li>
+            return <li><div className='post'>
+              {item}
+              </div></li>
           })
         }
       </ul>
--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,12 +1,24 @@
-import React from 'react'
+import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 
 function UserPage(props) {
     const id = useParams().id
+    const [user,setUser] = useState('user')
+      const [posts, setPosts] = useState(['the','overall','posts','for','this','user'])
+
   return (
     <div>
       <h1>The user profile for a particular user</h1>
       <h2>The id: {id}</h2>
+      <ul>
+        {
+          posts.map((item)=>{
+            return <li><div className='post'>
+              {item}
+              </div></li>
+          })
+        }
+      </ul>
     </div>
   )
 }
