2cd42430ea7c962320ea013ddff76f27ab2c98ee
Zachary Kuchar <zacharyk323@live.com>
2023-08-26 13:38:12 -0400

    Createing an new post component

---
+++ src/components/CreatePost.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function CreatePost() {
+  return (
+    <div>
+      <h1>This will be the form to create a new post</h1>
+    </div>
+  )
+}
+
+export default CreatePost
