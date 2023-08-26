3cd0440d82219c7841a36fb3e35a29386f6bf1a5
Zachary Kuchar <zacharyk323@live.com>
2023-08-26 13:41:48 -0400

    Form for creating posts

--- src/components/CreatePost.jsx
+++ src/components/CreatePost.jsx
@@ -4,6 +4,9 @@ function CreatePost() {
   return (
     <div>
       <h1>This will be the form to create a new post</h1>
+      <form>
+        <input name='post' type='text'/>
+      </form>
     </div>
   )
 }
