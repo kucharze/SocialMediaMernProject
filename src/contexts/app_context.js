7a3712611086ec04ee93176ced8ac090528d0fdb
Zachary Kuchar <zacharyk323@live.com>
2023-08-27 19:44:33 -0400

    Setup for posting to server

--- src/contexts/app_context.js
+++ src/contexts/app_context.js
@@ -3,13 +3,27 @@ import axios from "axios";
 
 export const AppContext = createContext();
 
+const BASE_URL = "http://localhost:3001";
+
 export const AppContextProvider = ({ children }) => {
   const [user, setUser] = useState(null);
   const [posts, setPosts] = useState(null);
 
-  const postData = (data) => {
+  const postData = async (data) => {
     console.log("Posting data");
     console.log(data);
+    // AXIOS
+    try {
+      const res = await axios.post(BASE_URL, data, {
+        headers: {
+          "Content-Type": "application/json",
+        },
+      });
+
+      console.log(res);
+    } catch (error) {
+      console.log("An error occurred: ", error);
+    }
   };
   return (
     <AppContext.Provider value={{ user, posts, postData }}>
