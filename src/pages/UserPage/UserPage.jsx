426dc7806de923ab7e1cb69ed15ecc933ef36767
Zachary Kuchar <zacharyk323@live.com>
2023-08-27 14:24:38 -0400

    Logic to only show create post button if user name matches id

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -1,10 +1,12 @@
 import React, {useState, useEffect} from 'react'
 import { useParams } from 'react-router-dom'
 import CreatePost from '../../components/CreatePost'
+import { useAuth } from '../../contexts/app_context'
 
 function UserPage(props) {
     const id = useParams().id
-    const [user,setUser] = useState('user')
+    const {user} = useAuth()
+    const [userName,setUserName] = useState('user')
     const [create,setCreate] = useState(false)
       const [posts, setPosts] = useState(['the','overall','posts','for','this','user'])
 
@@ -12,10 +14,17 @@ function UserPage(props) {
     <div>
       <h1>The user profile for a particular user</h1>
       <h2>The id: {id}</h2>
-      <button onClick={()=>{setCreate(!create)}}>Create a new post</button>
       {
-        create &&  <CreatePost/>
+        (user == 'Zack') && 
+        <div>
+        <button onClick={()=>{setCreate(!create)}}>Create a new post</button>
+        {
+          create &&  <CreatePost/>
+        }
+        </div>
+        
       }
+     
       <ul>
         {
           posts.map((item)=>{
