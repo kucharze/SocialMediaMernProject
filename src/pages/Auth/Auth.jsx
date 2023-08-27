e5c99fe2077f5b3f4d481939e269a89aa6d2af0c
Zachary Kuchar <zacharyk323@live.com>
2023-08-27 14:15:37 -0400

    Initial logic to create  signin

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -1,12 +1,20 @@
 import React, {useState} from 'react'
+import { useAuth } from '../../contexts/app_context'
 
 function Auth() {
+  const {postData} = useAuth()
+
+  const handleSubmit = (e) =>{
+    e.preventDefault()
+    console.log("Submitting")
+    postData()
+  }
   return (
     <div>
       <h1>Authentication page</h1>
-      <form>
-        <input name='email' type='text' placeholder='Email'/>
-        <input name='password' type='email' placeholder='password'/>
+      <form onSubmit={handleSubmit}>
+        <input name='email' type='email' placeholder='Email'/>
+        <input name='password' type='password' placeholder='password'/>
         <input type='submit'/>
       </form>
     </div>
