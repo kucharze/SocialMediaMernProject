672ef1e2b171d671e1a741bd9a579989b85ad219
Zachary Kuchar <zacharyk323@live.com>
2023-08-27 12:36:32 -0400

    Users schema setup

--- schemas/users.js
+++ schemas/users.js
@@ -1,1 +1,30 @@
 const mongoose = require("mongoose");
+
+const Users = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    email: { type: String, requried: true, lowercase: true },
+    password: { type: String, requried: true, minLength: 6 },
+    posts: { type: Object },
+  },
+  {
+    timestamps: true,
+    // Even though it's hashed - don't serialize the password
+    toJSON: {
+      transform: function (doc, ret) {
+        delete ret.password;
+        return ret;
+      },
+    },
+  }
+);
+
+Users.pre("save", async function (next) {
+  // 'this' is the user doc
+  if (!this.isModified("password")) return next();
+  // update the password with the computed hash
+  this.password = await bcrypt.hash(this.password, SALT_ROUNDS);
+  return next();
+});
+
+module.exports = mongoose.model("Users", Users);
