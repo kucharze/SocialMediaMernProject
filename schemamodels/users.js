af3294be38f9cf8788f7a46cbe56545138c99cbf
Zachary Kuchar <zacharyk323@live.com>
2023-08-28 18:43:40 -0400

    Unique keyword for emails

--- schemamodels/users.js
+++ schemamodels/users.js
@@ -3,7 +3,7 @@ const mongoose = require("mongoose");
 const Users = new mongoose.Schema(
   {
     name: { type: String, required: true },
-    email: { type: String, requried: true, lowercase: true },
+    email: { type: String, requried: true, lowercase: true, unique: true },
     password: { type: String, requried: true, minLength: 6 },
     posts: { type: mongoose.Schema.Types.ObjectId },
   },
