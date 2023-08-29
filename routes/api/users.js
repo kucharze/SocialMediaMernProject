46bee7e9c14f6a51e441b413194dd7dd2b78cd89
Zachary Kuchar <zacharyk323@live.com>
2023-08-29 11:36:05 -0400

    Initial setup for creating new user

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,9 +2,17 @@ const express = require("express");
 const router = express.Router();
 // const usersCtrl = require("../../controllers/api/users");
 
-router.post("/", (req, res) => {
+const User = require("../../schemamodels/users");
+
+router.post("/", async (req, res) => {
   console.log("Found a route for a post");
-  res.json("We are ok");
+  // res.json("We are ok");
+
+  try {
+    const user = await User.create(req.body);
+  } catch (error) {
+    res.status(400).json(error);
+  }
 });
 
 module.exports = router;
