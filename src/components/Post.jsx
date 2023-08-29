e46e416700f23acc327761032269f69aaac0c93f
Zachary Kuchar <zacharyk323@live.com>
2023-08-29 13:04:38 -0400

    Data for Post component

--- src/components/Post.jsx
+++ src/components/Post.jsx
@@ -1,9 +1,12 @@
 import React from 'react'
 
-function Post({user, post}) {
+function Post({user, post, isUser}) {
   return (
     <div>
       <h1>A single post</h1>
+      <h2>The user</h2>
+      <p>The data as part of the post: {post}</p>
+      <button>To Delete if this belongs to the logged in user</button>
     </div>
   )
 }
