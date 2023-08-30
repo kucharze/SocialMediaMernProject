23b999b17e181d73cd73e5283c1a4c6e2995243e
Zachary Kuchar <zacharyk323@live.com>
2023-08-29 20:26:59 -0400

    Logic to set and grab user token

--- src/contexts/app_context.js
+++ src/contexts/app_context.js
@@ -21,10 +21,34 @@ export const AppContextProvider = ({ children }) => {
       });
 
       console.log("The response", res);
+      localStorage.setItem("token", res.data);
     } catch (error) {
       console.log("An error occurred: ", error);
     }
   };
+
+  //Grab token from local storage
+  function getToken() {
+    // getItem returns null if there's no string
+    const token = localStorage.getItem("token");
+    if (!token) return null;
+    // Obtain the payload of the token
+    const payload = JSON.parse(atob(token.split(".")[1]));
+    //console.log(payload);
+    // A JWT's exp is expressed in seconds, not milliseconds, so convert
+    if (payload.exp < Date.now() / 1000) {
+      // Token has expired - remove it from localStorage
+      localStorage.removeItem("token");
+      return null;
+    }
+    return token;
+  }
+
+  function getUser() {
+    const token = getToken();
+    // If there's a token, return the user in the payload, otherwise return null
+    return token ? JSON.parse(atob(token.split(".")[1])).user : null;
+  }
   return (
     <AppContext.Provider value={{ user, posts, postData }}>
       {children}
