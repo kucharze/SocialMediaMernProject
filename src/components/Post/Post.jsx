a0670fc28d98df6af472ff4e6e8a8bce1344aa06
Zachary Kuchar <zacharyk323@live.com>
2023-08-31 12:16:56 -0400

    Additional data for is user post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,12 +1,21 @@
 import React from 'react'
+import { Link } from 'react-router-dom'
 
 function Post({user, post, isUser}) {
+  console.log('Post user',user)
   return (
-    <div>
+    <div className='Post'>
       <h1>A single post</h1>
-      <h2>The user</h2>
+      <h2>{user && user.screenName}</h2>
       <p>The data as part of the post: {post}</p>
-      <button>To Delete if this belongs to the logged in user</button>
+      {
+        isUser && <div className='editing'>
+          <h3>This only shows up if on the logged in User's page with their posts</h3>
+          <button>To Edit</button>
+          <button>To Delete</button>
+          </div>
+      }
+      
     </div>
   )
 }
