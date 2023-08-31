be6eccd0bb0c8c2427d5c4c290909d48a733333c
Zachary Kuchar <zacharyk323@live.com>
2023-08-31 14:44:35 -0400

    Logic to grab user data inside userpage

--- src/pages/UserPage/UserPage.jsx
+++ src/pages/UserPage/UserPage.jsx
@@ -6,34 +6,46 @@ import Post from '../../components/Post/Post'
 
 function UserPage(props) {
     const id = useParams().id
-    const {user} = useAuth()
+    const {user, searchUser} = useAuth()
     const [userName,setUserName] = useState('user')
     const [create,setCreate] = useState(false)
     const [posts, setPosts] = useState(['the','overall','posts','for','this','user'])
 
+    const grabUser = async() =>{
+      console.log("Bring in data for the current user page we are on")
+      const userdata = await searchUser(id)
+      console.log("userdata",userdata)
+      setUserName(userdata.screenName);
+    }
+
     useEffect(()=>{
-      console.log("This will bring in data for the current user page we are on")
+      if(user._id !== id){
+        grabUser()
+      }
+      else{
+        setUserName(user.screenName)
+      }
     },[])
   return (
     <div>
       <h1>The user profile for a particular user</h1>
+      <h2>{userName}</h2>
       {/* <h2>The id: {id}</h2> */}
       {
-        (user._id == id) && 
+        (user._id === id) && 
         <div>
         <button onClick={()=>{setCreate(!create)}}>Create a new post</button>
         {
           create &&  <CreatePost/>
         }
         </div>
-        
       }
      
       <ul className='postDisplay'>
         {
           posts.map((item,i)=>{
             return <li key={i}><div className='post'>
-              <Post user={user} post={item} isUser={user._id == id}/>
+              <Post user={user} post={item} isUser={user._id === id}/>
               </div></li>
           })
         }
