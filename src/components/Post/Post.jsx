306d976328b6e6221fefd7d328e0bacef0f6df16
Zachary Kuchar <zacharyk323@live.com>
2023-09-03 11:10:45 -0400

    Add state for post var

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,9 +1,11 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Posts.module.css'
+import axios from 'axios'
 
 function Post({user, post, isUser}) {
   //console.log('Post user',user)
+  const [postDesc, setPostDesc] = useState(post.post)
 
   const handleDelete = () =>{
     console.log("Trying to delete")
@@ -15,7 +17,7 @@ function Post({user, post, isUser}) {
   return (
     <div className={styles.Post}>
       <Link to={`/User/${user._id}`}><h1>{user && user.screenName}</h1></Link>
-      <p>My post: {post.post}</p>
+      <p>My post: {postDesc}</p>
       {
         isUser && <div className='editing'>
           <h3>This only shows up if on the logged in User's page with their posts</h3>
