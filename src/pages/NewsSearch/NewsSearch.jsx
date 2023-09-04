1d78b06143fd20ac552df78150ceabecf1e456ba
Zachary Kuchar <zacharyk323@live.com>
2023-09-04 17:34:27 -0400

    Adjusted styles

--- src/pages/NewsSearch/NewsSearch.jsx
+++ src/pages/NewsSearch/NewsSearch.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import axios from 'axios'
+import styles from './NewsSearch.module.css'
 
 //https://newsapi.org/v2/everything?q=tesla&from=2023-08-04&sortBy=publishedAt&apiKey=API_KEY
 function NewsSearch() {
@@ -7,13 +8,14 @@ function NewsSearch() {
     const [articles, setArticles] = useState(null)
 
     const searchNews = async () =>{
+        //Search for news articles based on a search term
         let api = `https://newsapi.org/v2/everything?q=${search}&from=2023-08-04&sortBy=publishedAt&apiKey=0539d4f171184511b8d1753b9a36dd72`
         let res = await axios.get(api)
         console.log(res.data)
         setArticles(res.data.articles)
     }
   return (
-    <div>
+    <div className={styles.NewsSearch}>
       <h1>News Search</h1>
       <input type='text' value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
       <button onClick={searchNews}>Search news</button>
@@ -24,7 +26,7 @@ function NewsSearch() {
         }
         {
             articles && articles.map((item)=>{
-                return <div className='news'>
+                return <div className={styles.news}>
                 <h1>{item.title}</h1>
                 <h2>Info</h2>
                 <p>{item.description}</p>
