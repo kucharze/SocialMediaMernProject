c30a774d47cb26f79c38a301073a50a53d305901
Zachary Kuchar <zacharyk323@live.com>
2023-09-04 13:26:57 -0400

    Logic to poll from api and display results

--- src/pages/NewsSearch/NewsSearch.jsx
+++ src/pages/NewsSearch/NewsSearch.jsx
@@ -1,11 +1,36 @@
-import React from 'react'
+import React, {useState} from 'react'
 import axios from 'axios'
 
 //https://newsapi.org/v2/everything?q=tesla&from=2023-08-04&sortBy=publishedAt&apiKey=API_KEY
 function NewsSearch() {
+    const[search, setSearch] = useState('')
+    const [articles, setArticles] = useState(null)
+
+    const searchNews = async () =>{
+        let api = `https://newsapi.org/v2/everything?q=tesla&from=2023-08-04&sortBy=publishedAt&apiKey=0539d4f171184511b8d1753b9a36dd72`
+        let res = await axios.get(api)
+        console.log(res.data)
+        setArticles(res.data.articles)
+    }
   return (
     <div>
       <h1>News Search</h1>
+      <input type='text' value={search} onChange={(e)=>{setSearch(e.target.value)}}/>
+      <button onClick={searchNews}>Search news</button>
+
+      <div>
+        {
+            articles && articles.map((item)=>{
+                return <>
+                <h1>Title:{item.title}</h1>
+                <h2>Info</h2>
+                <p>{item.desciption}</p>
+                <a href={item.url}>See link</a>
+                </>
+                
+            })
+        }
+      </div>
     </div>
   )
 }
