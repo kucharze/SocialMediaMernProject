5adc2586c0e912dbe2242b9c600e379c0a4538fe
Zachary Kuchar <zacharyk323@live.com>
2023-09-05 13:14:54 -0400

    Added picture to authentication page

--- src/pages/Auth/Auth.jsx
+++ src/pages/Auth/Auth.jsx
@@ -2,11 +2,13 @@ import React, {useState} from 'react'
 // import { useAuth } from '../../contexts/app_context'
 import SignInForm from '../../components/SigninForm/SignInForm'
 import LoginForm from '../../components/LoginForm/LoginForm'
+import k from "../../K 2.JPG";
 
 function Auth() {
 
   return (
     <div>
+      <img src={k} alt="" className="logopic" />
       <SignInForm/>
       <LoginForm/>
     </div>
