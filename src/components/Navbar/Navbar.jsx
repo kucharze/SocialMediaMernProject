6536f74d2d8f30a4da8266d77b136c75d66a8eb9
Zachary Kuchar <zacharyk323@live.com>
2023-09-05 13:31:07 -0400

    Refactored styles

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,11 +1,12 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../contexts/app_context'
+import styles from './Navbar.module.css'
 
 function Navbar() {
   const {user,logout} = useAuth()
   return (
-    <div className='navbar'>
+    <div className={styles.navbar}>
       <Link to={`/User/${user._id}`}>{user.screenName}'s page</Link>
       &nbsp;  &nbsp;
       <Link to={`/`}>🏠 Home</Link>
