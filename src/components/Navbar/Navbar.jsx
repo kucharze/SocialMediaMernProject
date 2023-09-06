807210ce82553f2bb3d01be803a8c61837ca03ab
Zachary Kuchar <zacharyk323@live.com>
2023-09-06 19:25:41 -0400

    Added new image

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -2,11 +2,13 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { useAuth } from '../../contexts/app_context'
 import styles from './Navbar.module.css'
+import Chat from './zchat02.gif'
 
 function Navbar() {
   const {user,logout} = useAuth()
   return (
     <div className={styles.navbar}>
+      <img src={Chat} alt="" className={styles.img}/>
       <Link to={`/User/${user._id}`}>{user.screenName}'s page</Link>
       &nbsp;  &nbsp;
       <Link to={`/`}>🏠 Home</Link>
