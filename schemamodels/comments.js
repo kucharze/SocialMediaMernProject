b4f8b61dbfd9f7d3ffd5aba63e38f054638da0d6
Zachary Kuchar <zacharyk323@live.com>
2023-10-02 14:27:52 -0400

    Initial addition of comment list for posts

---
+++ schemamodels/comments.js
@@ -0,0 +1,18 @@
+const mongoose = require("mongoose");
+const User = require("./users");
+
+const Comments = mongoose.Schema(
+  {
+    post: { type: mongoose.Schema.Types.ObjectId, required: true },
+    commentList: [
+      {
+        type: mongoose.Schema.Types.ObjectId,
+      },
+    ],
+  },
+  {
+    timestamps: true, //Tells when the timestamp has been created or last updated
+  }
+);
+
+module.exports = mongoose.model("Comments", Comments);
