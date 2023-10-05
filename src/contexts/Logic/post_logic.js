9520f66f445eb9eea701c910238caa4e6a2d7804
Zachary Kuchar <zacharyk323@live.com>
2023-10-05 14:32:02 -0400

    Updates to post logic, added axios import

--- src/contexts/Logic/post_logic.js
+++ src/contexts/Logic/post_logic.js
@@ -1,6 +1,36 @@
+import axios from "axios";
+
 export const usePosts = () => {
   const BASE_URL_POSTS = "http://localhost:3001/posts";
   // const BASE_URL_POSTS = "/posts";
+
+  // const createPost = async (post) => {
+  //   try {
+  //     setUser(
+  //       await postRequest({ id: user._id, post }, `${BASE_URL_POSTS}/newPost`)
+  //     );
+  //   } catch (error) {
+  //     console.log("error creating a post ", error);
+  //   }
+  // };
+
+  const updateLikes = async (point, id, user) => {
+    console.log(point);
+    try {
+      const res = await axios.put(
+        `${BASE_URL_POSTS}/updateLikes`,
+        { likes: point, postid: id, user: user },
+        {
+          headers: {
+            "Content-Type": "application/json",
+          },
+        }
+      );
+      console.log(res);
+    } catch (error) {
+      console.log("Error updating likes", error);
+    }
+  };
 
   const loadPosts = async () => {
     try {
